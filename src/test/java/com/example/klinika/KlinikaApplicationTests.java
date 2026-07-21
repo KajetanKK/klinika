79085e982bd9bf5469ffd7c1fb093fa06f78cd5c
Kajetan <knas.kajetan@gmail.com>
2026-07-21 13:45:29 +0200

    Szkielet projektu Spring Boot - klinika

---
+++ src/test/java/com/example/klinika/KlinikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.klinika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlinikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
